e78235ff69ee3b0c5b8edb57f64d4cada2a582c4
salih avcı <casus_salih_16@hotmail.com>
2024-04-12 21:31:12 +0300

    Herhangi bir komponent üzerinde children kullanımı ile alt html'ler üretimi sağlandı.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Button from "./Button";
 
 const messages = [
   "Learn React ⚛️",
@@ -37,18 +38,17 @@ function App() {
             Step {step}: {messages[step - 1]}
           </p>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            <Button
+              bgColor={"#7950f2"}
+              textColor={"#fff"}
               onClick={handlePrevious}
             >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+              <span>⬅️</span> Previous
+            </Button>
+
+            <Button bgColor={"#7950f2"} textColor={"#fff"} onClick={handleNext}>
+              Next <span>➡️</span>
+            </Button>
           </div>
         </div>
       )}
